7a89437e6eaeedd1efe1f58d0f4ad57db496341d
Mehul <mehulprajapati2802@gmail.com>
2024-11-10 19:13:12 -0500

    remove copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { FaGithub } from 'react-icons/fa'; // Import GitHub icon from react-icon
 
 function Footer() {
   return (
-    <footer className="bg-gradient-to-r from-purple-800 via-pink-800 to-red-800 text-white py-8 mt-16 shadow-lg">
+    <footer className="bg-gradient-to-r from-purple-800 via-pink-800 to-red-800 text-white py-8 mt-1 shadow-lg">
       <div className="container mx-auto text-center">
         <p className="text-sm md:text-base font-medium flex justify-center items-center space-x-2">
           <span>Made with ❤️ by <span className="font-semibold">Mehul</span></span>
@@ -13,15 +13,17 @@ function Footer() {
             href="https://github.com/mehul-m-prajapati/github_tracker"
             target="_blank"
             rel="noreferrer noopener"
-            className="inline-flex items-center text-purple-400 hover:text-purple-600 transition duration-300 ease-in-out"
+            className="inline-flex items-center text-white-400 hover:text-gray-600 transition duration-300 ease-in-out"
           >
             <FaGithub className="h-5 w-5 mr-2" /> {/* Using FaGithub icon */}
             GitHub Tracker
           </a>
         </p>
+
+        {/*
         <p className="text-sm md:text-base font-light mt-2">
-          &copy; 2024 <span className="font-semibold">My App</span>. All rights reserved.
-        </p>
+          &copy; 2024 <span className="font-semibold">GitHub Tracker</span>. All rights reserved.
+        </p>*/}
       </div>
     </footer>
   );
