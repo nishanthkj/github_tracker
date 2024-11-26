f64d863451b4865469ceb3ef6d5a9c55bbc869fa
Mehul <mehulprajapati2802@gmail.com>
2024-11-25 20:18:39 -0500

    modified comments

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -1,6 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
-import Home from "../pages/Home/Home"; // Import the Home (Dashboard) component
+import Home from "../pages/Home/Home"; // Import the Home component
 import About from "../pages/About/About"; // Import the About component
 import Contact from "../pages/Contact/Contact"; // Import the Contact component
 import Contributors from "../pages/Contributors/Contributors";
@@ -8,7 +8,7 @@ import Contributors from "../pages/Contributors/Contributors";
 const Router = () => {
   return (
     <Routes>
-      {/* Redirect from root (/) to the dashboard */}
+      {/* Redirect from root (/) to the home page */}
       <Route path="/" element={<Navigate to="/home" replace />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
