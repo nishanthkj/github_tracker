34c514c915ba9e7e995beaa61a899f06c6675ae5
Trailblazer09 <akaaj15o@gmail.com>
2025-07-25 20:47:33 +0530

    reversed the code to orginal

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -8,26 +8,19 @@ import Login from "../pages/Login/Login.tsx"
 import UserProfile from "../pages/UserProfile/UserProfile.tsx"
 
 
-interface RouterProps {
-  theme: string;
-}
 
-const Router: React.FC<RouterProps> = ({ theme }) => {
-
-
+const Router = () => {
   return (
     <Routes>
       {/* Redirect from root (/) to the home page */}
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/" element={<Navigate to="/home" replace />} />
-      <Route path="/home" element={<Home key={theme} theme={theme} />} />
+      <Route path="/home" element={<Home theme="dark" />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/contributors" element={<Contributors />} />
       <Route path="/user/:username" element={<UserProfile />} />
     </Routes>
   );
-};
-
+  };
 export default Router;
