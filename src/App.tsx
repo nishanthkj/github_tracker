3f72b0ee85c4e2d26daab4e2d8b149d840472564
Trailblazer09 <akaaj15o@gmail.com>
2025-07-25 20:01:33 +0530

    remove extra code from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,20 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ScrollProgressBar from './components/ScrollProgressBar';
 import { Toaster } from "react-hot-toast";
-import { useTheme } from "./hooks/useTheme";
-
-import Router from "./Routes/Router";
+import Router from "./Routes/Router.tsx";
 
 function App() {
-  const { theme, setTheme } = useTheme();
-
   return (
 
-      <div className="relative flex flex-col min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white">
+      <div className="relative flex flex-col min-h-screen">
         <ScrollProgressBar/>
 
         {/* Navbar */}
         <Navbar />
 
         {/* Main content */}
-        <main className="flex-grow bg-gray-50 dark:bg-gray-800 flex justify-center items-center">
-          <Router theme={theme} />
+        <main className="flex-grow bg-gray-50 flex justify-center items-center">
+        <Router/>
         </main>
 
         {/* Footer */}
