207a6dab9ecb08153ab53dfcfc7b1af4bef5fe8a
Trailblazer09 <akaaj15o@gmail.com>
2025-07-26 07:22:23 +0530

    reversed the code to original

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,13 @@
-// main.tsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "@mui/material/styles";
-import { getMuiTheme } from "./theme";
-import { useTheme } from "./hooks/useTheme";
-
-const RootApp = () => {
-  const { theme } = useTheme();
-  return (
-    <ThemeProvider theme={getMuiTheme(theme)}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
-  );
-};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RootApp />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
