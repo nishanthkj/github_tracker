fd77be9b5d808edfee886fb7dfe5d79dafd8c164
Mehul <mehulprajapati2802@gmail.com>
2025-07-27 17:20:23 +0530

    redirect to home

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -15,7 +15,7 @@ const Router = () => {
       {/* Redirect from root (/) to the home page */}
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/home" element={<Home theme="dark" />} />
+      <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/contributors" element={<Contributors />} />
